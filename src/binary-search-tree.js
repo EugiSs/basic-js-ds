48b5e9daddf98040b967166885927fc42c47d556
Eugi <neskisun@gmail.com>
2023-12-23 01:21:41 +0300

    feat: complete binary and list tasks

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,129 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js")
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js")
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
+
 class BinarySearchTree {
+	constructor() {
+		this.treeRoot = null
+	}
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	root() {
+		return this.treeRoot
+	}
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	add(data) {
+		this.treeRoot = addNode(this.treeRoot, data)
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		function addNode(current, data) {
+			if (!current) return new Node(data)
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+			if (current.data === data) return current
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+			if (current.data > data) {
+				current.left = addNode(current.left, data)
+			} else {
+				current.right = addNode(current.right, data)
+			}
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+			return current
+		}
+	}
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	has(data) {
+		return hasValue(this.treeRoot, data)
+		function hasValue(current, data) {
+			if (!current) return false
+
+			if (current.data === data) return true
+
+			return current.data > data
+				? hasValue(current.left, data)
+				: hasValue(current.right, data)
+		}
+	}
+
+	find(data) {
+		// throw new NotImplementedError("Not implemented")
+		return findValue(this.treeRoot, data)
+
+		function findValue(current, data) {
+			if (!current) return null
+
+			if (current.data === data) return current
+
+			if (data < current.data) {
+				return findValue(current.left, data)
+			} else {
+				return findValue(current.right, data)
+			}
+		}
+	}
+
+	remove(data) {
+		this.treeRoot = removeNode(this.treeRoot, data)
+
+		function removeNode(current, data) {
+			if (!current) return null
+
+			if (data < current.data) {
+				current.left = removeNode(current.left, data)
+				return current
+			} else if (data > current.data) {
+				current.right = removeNode(current.right, data)
+				return current
+			} else {
+				if (!current.left && !current.right) {
+					return null
+				}
+				if (!current.right) {
+					current = current.left
+					return current
+				}
+				if (!current.left) {
+					current = current.right
+					return current
+				}
+
+				let maxLeftValue = current.left
+				while (maxLeftValue.right) {
+					maxLeftValue = maxLeftValue.right
+				}
+				current.data = maxLeftValue.data
+				current.left = removeNode(current.left, maxLeftValue.data)
+
+				return current
+			}
+		}
+	}
+
+	min() {
+		if (!this.treeRoot) return null
+		let current = this.treeRoot
+		while (current.left) {
+			current = current.left
+		}
+		return current.data
+	}
+
+	max() {
+		if (!this.treeRoot) return null
+		let current = this.treeRoot
+		while (current.right) {
+			current = current.right
+		}
+		return current.data
+	}
 }
 
+let b = new BinarySearchTree()
+b.has("test")
+console.log(b)
+
 module.exports = {
-  BinarySearchTree
-};
+	BinarySearchTree
+}
