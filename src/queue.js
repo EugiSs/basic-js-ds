8c8ab614c7005d6a21825b77c1eadf9481a45206
Eugi <neskisun@gmail.com>
2023-12-23 02:15:23 +0300

    feat: complete queue and stack tasks

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js")
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js")
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,24 +13,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+// class Queue {
+// 	constructor() {
+// 		this.list = null
+// 	}
+
+// 	getUnderlyingList() {
+// 		return this.list
+// 	}
+
+// 	enqueue(value) {
+// 		let node = new ListNode(value)
+// 		if (!this.list) return (this.list = node)
+// 		let current = this.list
+// 		while (current.next) {
+// 			current = current.next
+// 		}
+// 		current.next = node
+// 	}
+
+// 	dequeue() {
+// 		let val = this.list.value
+// 		this.list = this.list.next
+// 		return val
+// 	}
+// }
+
 class Queue {
+	constructor() {
+		this.list = null
+	}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	getUnderlyingList() {
+		return this.list
+	}
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	enqueue(value) {
+		let elem = new ListNode(value)
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		if (!this.list) return (this.list = elem)
+
+		let current = this.list
+		while (current.next) {
+			current = current.next
+		}
+
+		current.next = elem
+	}
+
+	dequeue() {
+		let elemValue = this.list.value
+		this.list = this.list.next
+		return elemValue
+	}
 }
 
 module.exports = {
-  Queue
-};
+	Queue
+}
